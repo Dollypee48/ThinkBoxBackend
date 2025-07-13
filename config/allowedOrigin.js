7e7bc4dcdf78f3eaf111b4a8dc1830d87a156632
Dollypee48 <akinolad22@gmail.com>
2025-07-13 17:08:06 +0100

    change the url

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://thinkbox-frontend-git-main-dollypees-projects-7c344058.vercel.app/"
+    "https://thinkbox-frontend-git-main-dollypees-projects-7c344058.vercel.app"
 ];
 
 module.exports = allowedOrigins;
